baf0e91f22728ff4d75b4b9270fb2d3aa1096b0b
weixuanteo <weixuanteo@gmail.com>
2021-04-02 00:04:13 +0800

    add rabbitmq implementation to place order service

--- create_order/create_order.py
+++ create_order/create_order.py
@@ -4,6 +4,8 @@ import requests
 import json
 from os import environ
 import logging
+import amqp_setup
+import pika
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -26,15 +28,38 @@ def create_new_order():
     # new_order_data = {"rest_id": rest_id, "order_type": order_type, "comments": comments,'order_items':order_items,'table_no':table_no}
     # print(json.dumps(new_order_data))
     response = requests.post("http://order-service:5000/order/neworder", json=data)
-
     if response.status_code != 201:
         return jsonify(
             {
                 "status": "error",
-                "message": "error in calling restaurant service"
+                "message": "error in calling order service"
             }
         )
+
+    order_id = response.json()["data"]["order_id"]
+    order_status = response.json()["data"]["order_status"]["status"]
+
+    queue_name = "order" + str(order_id)
+    routing_key = str(order_id) + ".order.status"
+    message_data = json.dumps({"order_id": order_id, "status": order_status})
+
+    try:
+        amqp_setup.channel.queue_declare(queue=queue_name, durable=True)
+        amqp_setup.channel.queue_bind(exchange=amqp_setup.exchangename, queue=queue_name, routing_key=routing_key)
+        amqp_setup.channel.basic_publish(exchange=amqp_setup.exchangename, routing_key=routing_key, body=message_data, properties=pika.BasicProperties(delivery_mode=2))
+        print("\nOrder notification published to the RabbitMQ Exchange:", message_data)
+
+    except Exception as e:
+        print(e)
+        return jsonify(
+            {
+                "status": "error",
+                "message": "error in publishing message to rabbitmq"
+            }
+        )
+
     
+
     return jsonify(
         {
             "status": "success",
