d9968765d8b11291bb7536160c2d325615e044e1
JiayuMeng <JiayuMeng@users.noreply.github.com>
2021-04-02 18:00:41 +0800

    admin create rest UI successfully

--- AdminUI/uploadRes.js
+++ AdminUI/uploadRes.js
@@ -8,14 +8,26 @@ const createRes = (resInfo) => {
         .catch(error => console.error(error));
 };
 
+var switchStatus = false;
+$("#isopen").on('change', function() {
+    if ($(this).is(':checked')) {
+        switchStatus = $(this).is(':checked');
+        // alert(switchStatus);// To verify
+    }
+    else {
+       switchStatus = $(this).is(':checked');
+    //    alert(switchStatus);// To verify
+    }
+});
+console.log(switchStatus)
 const form = document.querySelectorAll('form')[1];
 
 const formEvent = form.addEventListener('submit', event => {
     event.preventDefault();
-
     const name = document.querySelector('#resName').value;
-    const is_open = Boolean(document.querySelector('#isopen').value);
+    const is_open = switchStatus
     const address = document.querySelector('#resAddress').value;
+    console.log(is_open)
 
     const resInfo = {name, is_open , address};
     createRes(resInfo);
