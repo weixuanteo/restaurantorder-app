9768a2d7635186513e912bdfa80c191aaa9aafe7
JiayuMeng <JiayuMeng@users.noreply.github.com>
2021-04-04 23:21:28 +0800

    update create_restaurant API to dynamically retrieve ownerid

--- AdminUI/uploadRes.js
+++ AdminUI/uploadRes.js
@@ -1,38 +1,64 @@
-//Upload Restaurant 
-const createRes = (resInfo) => {
-    // RMB to change the oid variable 
-    axios.post('http://localhost/create_restaurant/1', resInfo)
-        .then(response => {
-            const addedRes = response.data;
-            console.log(`POST: Restaurant is added`, addedRes);
-        })
-        .catch(error => console.error(error));
-};
+window.onload = function() {
+    const owner = getOwner();
+    const owner_name = owner.name;
+    document.getElementById("ownerName").innerHTML = owner_name;
 
-var switchStatus = false;
-$("#isopen").on('change', function() {
-    if ($(this).is(':checked')) {
-        switchStatus = $(this).is(':checked');
-        // alert(switchStatus);// To verify
+    attachSignOut();
+}
+
+
+new Vue({
+    el: "#app",
+    data: {
+        owner_id: 0,
+    },
+    mounted: function() {
+        const owner = getOwner();
+        this.owner_id = owner.owner_id;
+        console.log(this.owner_id);
+
+        //Upload Restaurant 
+        const createRes = (resInfo) => {
+            // RMB to change the oid variable 
+            axios.post('http://localhost/create_restaurant/'+this.owner_id, resInfo)
+                .then(response => {
+                    const addedRes = response.data;
+                    console.log(`POST: Restaurant is added`, addedRes);
+                })
+                .catch(error => console.error(error));
+        };
+
+        var switchStatus = false;
+        $("#isopen").on('change', function() {
+            if ($(this).is(':checked')) {
+                switchStatus = $(this).is(':checked');
+                // alert(switchStatus);// To verify
+            }
+            else {
+            switchStatus = $(this).is(':checked');
+            //    alert(switchStatus);// To verify
+            }
+        });
+        console.log(switchStatus)
+        const form = document.querySelectorAll('form')[1];
+
+        const formEvent = form.addEventListener('submit', event => {
+            event.preventDefault();
+            const name = document.querySelector('#resName').value;
+            const is_open = switchStatus
+            const address = document.querySelector('#resAddress').value;
+            console.log(is_open)
+
+            const resInfo = {name, is_open , address};
+            createRes(resInfo);
+            console.log(resInfo)
+        });
+
+
     }
-    else {
-       switchStatus = $(this).is(':checked');
-    //    alert(switchStatus);// To verify
-    }
-});
-console.log(switchStatus)
-const form = document.querySelectorAll('form')[1];
+})
 
-const formEvent = form.addEventListener('submit', event => {
-    event.preventDefault();
-    const name = document.querySelector('#resName').value;
-    const is_open = switchStatus
-    const address = document.querySelector('#resAddress').value;
-    console.log(is_open)
 
-    const resInfo = {name, is_open , address};
-    createRes(resInfo);
-    console.log(resInfo)
-});
+
 
 
