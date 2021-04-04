6f7f4d34368feebe22dc5149bfd564a276b06d9c
weixuanteo <weixuanteo@gmail.com>
2021-04-04 17:22:58 +0800

    Update autoload.js
    add signout and getowner utility functions

--- AdminUI/assets/js/autoload.js
+++ AdminUI/assets/js/autoload.js
@@ -4,3 +4,20 @@ const owner = localStorage.getItem("owner");
 if (!owner) {
     window.location.href = "login.html";
 }
+
+function getOwner() {
+    const owner = JSON.parse(localStorage.getItem("owner"));
+    return owner;
+}
+
+function signOut() {
+    localStorage.removeItem("owner");
+    window.location.href = "login.html";
+}
+
+function attachSignOut() {
+    const logoutBtn = document.getElementById("logoutBtn");
+    logoutBtn.addEventListener("click", function() {
+        signOut();
+    })
+}
